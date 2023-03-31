e1227f38c26e1838bcfdc6d55b31d0157dbedc8c
Amal Kuriakose <amalkuriakose802@gmail.com>
2023-03-31 14:50:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import SignIn from './components/SignIn/SignIn'
+import Register from './components/Register/Register'
+import ParticlesBg from 'particles-bg';
+import { useState } from 'react';
+
+const returnClarifyRequestOptions = (image) => {
+  const raw = JSON.stringify({
+    "user_app_id": {
+      "user_id": "<your-user-id>",
+      "app_id": "<your-app-id>"
+    },
+    "inputs": [
+      {
+        "data": {
+          "image": {
+            "url": image
+          }
+        }
+      }
+    ]
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key <your-key>'
+    },
+    body: raw
+  };
+
+  return requestOptions;
+}
 
 function App() {
+  const [input, setInput] = useState('')
+  const [imageUrl, setImageUrl] = useState('')
+  const [box, setBox] = useState({})
+  const [route, setRoute] = useState('signin')
+  const [isSignedIn, setIsSignedIn] = useState(false)
+  const [user, setUser] = useState({ id: '', name: '', email: '', entries: 0, joined: '' })
+
+  const loadUser = (data) => {
+    setUser({
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    })
+  }
+
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  const onInputChange = (event) => {
+    setInput(event.target.value)
+  }
+
+  const onButtonSubmit = () => {
+    setImageUrl(input)
+    detectFace()
+  }
+
+  async function detectFace() {
+    try {
+      const response = await fetch(`https://api.clarifai.com/v2/models/face-detection/outputs`, returnClarifyRequestOptions(imageUrl))
+      const data = await response.json()
+      if (response.status === 200) {
+        await fetch('http://localhost:4000/image', {
+          method: 'put',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            id: user.id
+          })
+        })
+          .then(response => response.json()
+          )
+          .then(count => {
+            setUser(prevState => ({ ...prevState, entries: count }))
+          })
+
+      }
+      setBox(calculateFaceLocation(data))
+    } catch (error) {
+      console.log('Error', error)
+      alert('Try again')
+    }
+  }
+
+  const onRouteChange = (route) => {
+    if (route === 'signout') {
+      setIsSignedIn(false)
+    } else if (route === 'home') {
+      setIsSignedIn(true)
+    }
+    setRoute(route)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ParticlesBg type="fountain" bg={true} />
+      <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange} />
+      {
+        route === 'home' ?
+          <div>
+            <Logo />
+            <Rank
+              name={user.name}
+              entries={user.entries}
+            />
+            <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onButtonSubmit} />
+            <FaceRecognition imageUrl={imageUrl} box={box} />
+          </div>
+          : (route === 'signin' ?
+            <SignIn loadUser={loadUser} onRouteChange={onRouteChange} />
+            : <Register loadUser={loadUser} onRouteChange={onRouteChange} />
+          )
+      }
     </div>
   );
 }
